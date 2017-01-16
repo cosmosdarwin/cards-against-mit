375017ba2eab7e20d63f45dfbb6d392a6a7644ba
Cosmos Darwin <cosdar@microsoft.com>
2017-01-16 17:12:59 -0500

    specify database name in connectionString

--- bootstrap-deck.js
+++ bootstrap-deck.js
@@ -5,7 +5,7 @@
 var fs = require('fs');
 
 // copy the connection string from the Azure portal
-const connectionString = "mongodb://cards-against-mit:EUg4r8NC7Vvsr2NGbRGPmHEtylu6VJKGxchWCUmzfUdnTSTswcOcJS9kCLgNyzAOlUzsNz3b2gznIPGaM5W9wg==@cards-against-mit.documents.azure.com:10250/?ssl=true&sslverifycertificate=false";
+const connectionString = "mongodb://cards-against-mit:EUg4r8NC7Vvsr2NGbRGPmHEtylu6VJKGxchWCUmzfUdnTSTswcOcJS9kCLgNyzAOlUzsNz3b2gznIPGaM5W9wg==@cards-against-mit.documents.azure.com:10250/cardsdatabase?ssl=true&sslverifycertificate=false";
 
 // access remote database
 const db = require("monk")(connectionString);
@@ -15,9 +15,9 @@ db.then(() => {
   console.log("Successfully connected to the remote database!");
 
   // create collections
-  var WhiteCardCollection = db.create('whitecards');
-  var BlackCardCollection = db.create('blackcards');
-  console.log("Successfully created both collections!");
+  var WhiteCardCollection = db.get('whitecards');
+  var BlackCardCollection = db.get('blackcards');
+  console.log("Successfully retrieved both collections!");
 
   // insert docs into collections...
 
@@ -30,6 +30,7 @@ db.then(() => {
     for (var i = 0; i < blackCards.length; i++) {
       // insert each one into collection in the remote database
       BlackCardCollection.insert(blackCards[i]);
+      console.log("Inserted " + blackCards[i].string);
     }
     console.log("Successfully inserted all black cards!");
   });
@@ -43,6 +44,7 @@ db.then(() => {
     for (var i = 0; i < whiteCards.length; i++) {
       // insert each one into collection in the remote database
       BlackCardCollection.insert(whiteCards[i]);
+      console.log("Inserted " + whiteCards[i].string);
     }
     console.log("Successfully inserted all white cards!");
   });
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'jade');
 // Mongo Database
 var mongo = require('mongodb');
 var ObjectID = require('mongodb').ObjectID;
-const connectionString = "mongodb://cards-against-mit:EUg4r8NC7Vvsr2NGbRGPmHEtylu6VJKGxchWCUmzfUdnTSTswcOcJS9kCLgNyzAOlUzsNz3b2gznIPGaM5W9wg==@cards-against-mit.documents.azure.com:10250/admin?ssl=true&sslverifycertificate=false";
+const connectionString = "mongodb://cards-against-mit:EUg4r8NC7Vvsr2NGbRGPmHEtylu6VJKGxchWCUmzfUdnTSTswcOcJS9kCLgNyzAOlUzsNz3b2gznIPGaM5W9wg==@cards-against-mit.documents.azure.com:10250/cardsdatabase?ssl=true&sslverifycertificate=false";
 const db = require("monk")(connectionString);
 
 // MIDDLEWARE
