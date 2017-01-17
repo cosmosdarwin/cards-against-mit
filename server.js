bd0163c32a1692f7f1b6395a97e126b4fa099784
Cosmos Darwin <cosdar@microsoft.com>
2017-01-17 11:33:47 -0500

    code change 1/2 for rehearsal

--- server.js
+++ server.js
@@ -191,9 +191,6 @@ app.use(function(req, res, next) {
 
 // RUN
 
-app.listen(80);
-
-/*
 if (process.env.NODE_ENV == "production") {
   // in Azure cloud
   app.listen(process.env.PORT);
@@ -202,4 +199,3 @@ else {
   // local
   app.listen(80);
 }
-*/
